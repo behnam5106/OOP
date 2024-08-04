c51dcd5b72c3e0e490f421f71b948816327bf1e6
behnam5106 <47812522+behnam5106@users.noreply.github.com>
2024-08-04 17:43:08 +0200

    add setter and getter

--- learning_class.cpp
+++ learning_class.cpp
@@ -33,6 +33,45 @@ public:
         Age = age;
     }
 };
+
+class Employee_1 {
+private:
+    string Name;
+    string Company;
+    int Age;
+
+public:
+    void setName(string name){//setter
+        Name = name;
+    }
+    string getName(){//getter
+        return Name;
+    }
+    void setCompany(string company){//setter
+        Company = company;
+    }
+    string getCompany(){//getter
+        return Company;
+    }
+    void setAge(int age){//setter
+        Age = age;
+    }
+    int getAge(){//getter
+        return Age;
+    }
+    void IntroduceYourself(){
+        std::cout << "Name - " << Name << std::endl;
+        std::cout << "Company - " << Company << std::endl;
+        std::cout << "Age - " << Age << std::endl;
+    }
+    // defining constructor
+    Employee_1(string name, string company, int age){
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+};
+
 int main()
 {
     Employee employee_1;
@@ -51,6 +90,11 @@ int main()
     EmployeeWithConstructor employee_3 = EmployeeWithConstructor("Ant", "DDS3", 25);
     employee_3.IntroduceYourself();
 
+    Employee_1 employee_4 = Employee_1("Ali", "DDA", 26);
+    employee_4.IntroduceYourself();
+
+    employee_4.setAge(99);
+    std::cout<< employee_4.getName()<<" is "<<employee_4.getAge()<<" years old."<<std::endl;
 
 
  }
